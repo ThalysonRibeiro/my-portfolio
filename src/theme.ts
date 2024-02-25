1a5ff41fb83aae4580dc81d10a129944e218c4db
THALYSON RAFAEL <rafinha.head@gmail.com>
2024-02-25 02:42:12 -0400

    Features:material-ui theme, CssBaseline

---
+++ src/theme.ts
@@ -0,0 +1,29 @@
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+
+let theme = createTheme({
+    palette: {
+        primary: {
+            main: '#232323',
+        },
+        secondary: {
+            main: '#8000FF',
+        },
+    },
+    typography: {
+        fontFamily: "Segoe UI"
+        // '-apple-system',
+        // 'BlinkMacSystemFont',
+        // '"Segoe UI"',
+        // 'Roboto',
+        // '"Helvetica Neue"',
+        // 'Arial',
+        // 'sans-serif',
+        // '"Apple Color Emoji"',
+        // '"Segoe UI Emoji"',
+        // '"Segoe UI Symbol"',
+    }
+});
+
+theme = responsiveFontSizes(theme);
+
+export default theme;
