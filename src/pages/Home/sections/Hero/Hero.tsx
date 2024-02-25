51698b2b396d4070bdf3e4bc996977d2a5fe2f16
THALYSON RAFAEL <rafinha.head@gmail.com>
2024-02-25 04:30:28 -0400

    Criados e atualizados

---
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -0,0 +1,68 @@
+import { Box, Container, Grid, Typography, styled } from "@mui/material"
+import Avatar from "../../../../assets/images/img-eu.jpg"
+import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import StyledButton from "../../../../components/StyletButton/StyledButton";
+import { AnimatedBackground } from "../../../../components/StyletButton/AnimatedBackground/AnimatedBackground";
+
+const Hero = () => {
+
+    const StyledHero = styled("div")(({ theme }) => ({
+        backgroundColor: theme.palette.primary.main,
+        height: "100vh",
+        display: "flex",
+        alignItems: "center "
+    }))
+
+    const StyledImg = styled("img")(({ theme }) => ({
+        width: "80%",
+        borderRadius: "50%",
+        border: `1px solid ${theme.palette.primary.contrastText}`
+    }))
+
+    return (
+        <>
+            <StyledHero>
+                <Container>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} md={5}>
+                            <Box position="relative">
+                                <Box position="absolute" width={"150%"} top={-100} right={0}>
+                                    <AnimatedBackground />
+                                </Box>
+                                <Box position="absolute" textAlign="center">
+                                    <StyledImg src={Avatar} />
+                                </Box>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} md={7}>
+                            <Typography color="primary.contrastText" variant="h1" textAlign={"center"} pb={2}>Thalyson Ribeiro</Typography>
+                            <Typography color="primary.contrastText" variant="h2" textAlign={"center"}>Eu sou engenheiro de software</Typography>
+                            <Grid container display={"flex"} justifyContent={"center"} spacing={3} pt={3}>
+                                <Grid item xs={12} md={4} display={"flex"} justifyContent={"center"}>
+
+                                    <StyledButton>
+                                        <CloudDownloadIcon />
+                                        <Typography>
+                                            Download CV
+                                        </Typography>
+                                    </StyledButton>
+                                </Grid>
+                                <Grid item xs={12} md={4} display={"flex"} justifyContent={"center"}>
+                                    <StyledButton>
+                                        <MailOutlineIcon />
+                                        <Typography>
+                                            Contate-me
+                                        </Typography>
+                                    </StyledButton>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </StyledHero>
+        </>
+    )
+}
+
+export default Hero
